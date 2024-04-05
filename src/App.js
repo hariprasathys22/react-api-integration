7d7f5c19862a81f47236149f58b653fbf25894ab
hariprasathys22 <confluenceinfotechsolutions@gmail.com>
2024-04-05 18:55:52 +0530

    Completed the edit and delete functionality

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 import Products from './components/Products';
 
-import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SingleProduct from './components/SingleProduct';
 
 function App() {
+  const isRootPath = ()=>{
+    return window.location.pathname === "/"
+  }
   return (
     <BrowserRouter>
     
@@ -13,7 +16,7 @@ function App() {
       
       <Route exact path='/product' element={<Products/>} />
       <Route exact path='/product/:id' element={<SingleProduct />}/>
-   
+      <Route path="/" element={isRootPath()?<Navigate to="/product"/>:<Products />}/>
 
       
     </Routes>
